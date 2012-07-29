9abdbc1a2231d2775b073e66783e9c0f4c1269ad
Timo Bretten <t.bretten@gmail.com>
2012-07-29 09:36:55 +0300

    Abstract v1 done

---
+++ presentation/auto/_region_.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (TeX-run-style-hooks
+     "microtype"
+     "geometry"
+     "babel"
+     "english"
+     "latex2e"
+     "art10"
+     "article"
+     "a4paper")))
+
---
+++ presentation/auto/abstract.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "abstract"
+ (lambda ()
+    (TeX-run-style-hooks
+     "microtype"
+     "geometry"
+     "babel"
+     "english"
+     "latex2e"
+     "art10"
+     "article"
+     "a4paper")))
+
